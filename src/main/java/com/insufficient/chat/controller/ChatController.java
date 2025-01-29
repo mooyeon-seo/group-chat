6d31af1365ec71894d75aaf06faf5cc261bfd797
mooyeon-seo <seomooyeon@gmail.com>
2025-01-29 22:27:03 +0900

    feat: update ChatController to use MessageDTO for message handling

--- src/main/java/com/insufficient/chat/controller/ChatController.java
+++ src/main/java/com/insufficient/chat/controller/ChatController.java
@@ -5,14 +5,14 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
-import com.insufficient.chat.model.Message;
+import com.insufficient.chat.dto.MessageDTO;
 
 @Controller
 public class ChatController {
 
-    @MessageMapping("/send/{groupId}")
-    @SendTo("/topic/group/{groupId}")
-    public Message sendMessage(@Payload Message message) {
-        return message;
+    @MessageMapping("/send/{groupId}") // Clients send messages here
+    @SendTo("/topic/group/{groupId}") // Broadcast to all users in the group
+    public MessageDTO sendMessage(@Payload MessageDTO message) {
+        return message; // Message is sent to all users in the group
     }
 }
